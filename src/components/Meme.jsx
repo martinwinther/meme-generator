b4cc3faf88991d0d7abf9645f80debc0f5126bf8
Martin Winther <mswinther@icloud.com>
2023-06-26 13:43:29 +0200

    new state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,33 @@
+import memesData from "../memesData";
+import React from "react";
+
 export default function Meme() {
+	const [meme, setMeme] = React.useState({
+		topText: "",
+		bottomText: "",
+		randomImage: "http://i.imgflip.com/1bij.jpg",
+	});
+	const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+	function getMemeImage() {
+		const memesArray = memesData.data.memes;
+		const randomNumber = Math.floor(Math.random() * memesArray.length);
+		const url = memesArray[randomNumber].url;
+		setMeme((prevMeme) => ({
+			...prevMeme,
+			randomImage: url,
+		}));
+	}
 	return (
 		<main>
-			<form className="form">
+			<div className="form">
 				<input className="form--input" placeholder="Top Text" type="text" />
 				<input className="form--input" placeholder="Bottom Text" type="text" />
-				<button className="form--button">Get a new meme image 🖼</button>
-			</form>
+				<button className="form--button" onClick={getMemeImage}>
+					Get a new meme image 🖼
+				</button>
+			</div>
+			<img src={meme.randomImage} className="meme--image" />
 		</main>
 	);
 }
