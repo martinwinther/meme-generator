61ab8244b3019935171a1573e79564638efaaf0f
Martin Winther <mswinther@icloud.com>
2023-07-01 12:05:29 +0200

    Form function

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,5 +1,5 @@
-import memesData from "../memesData";
 import React from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
 	const [meme, setMeme] = React.useState({
@@ -10,7 +10,7 @@ export default function Meme() {
 	const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
 	function getMemeImage() {
-		const memesArray = memesData.data.memes;
+		const memesArray = allMemeImages.data.memes;
 		const randomNumber = Math.floor(Math.random() * memesArray.length);
 		const url = memesArray[randomNumber].url;
 		setMeme((prevMeme) => ({
@@ -18,16 +18,43 @@ export default function Meme() {
 			randomImage: url,
 		}));
 	}
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setMeme((prevMeme) => ({
+			...prevMeme,
+			[name]: value,
+		}));
+	}
+
 	return (
 		<main>
 			<div className="form">
-				<input className="form--input" placeholder="Top Text" type="text" />
-				<input className="form--input" placeholder="Bottom Text" type="text" />
+				<input
+					type="text"
+					placeholder="Top text"
+					className="form--input"
+					name="topText"
+					value={meme.topText}
+					onChange={handleChange}
+				/>
+				<input
+					type="text"
+					placeholder="Bottom text"
+					className="form--input"
+					name="bottomText"
+					value={meme.bottomText}
+					onChange={handleChange}
+				/>
 				<button className="form--button" onClick={getMemeImage}>
-					Get a new meme image
+					Get a new meme image 🖼
 				</button>
 			</div>
-			<img src={meme.randomImage} className="meme--image" />
+			<div className="meme">
+				<img src={meme.randomImage} className="meme--image" />
+				<h2 className="meme--text top">{meme.topText}</h2>
+				<h2 className="meme--text bottom">{meme.bottomText}</h2>
+			</div>
 		</main>
 	);
 }
